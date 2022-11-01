f866ca0df0cfcf9967b5665d696d4c8c2c52a69d
AntonTihonov0202 <anton.tixonov.1998@mail.ru>
2022-11-01 12:23:32 +0300

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+      xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello world</title>
+    <link rel="stylesheet" href="css/style.css"
+</head>
+<body>
+<h1>Hello</h1>
+    <form action="php/mail.php" method="post">
+        <fieldset>
+            <legend>Поля для заполнения</legend>
+            <label> Ваше имя: <input type="text" name="user_name" placeholder="Имя"</label>
+            <label> Ваш email: <input type="email" name="user_email" placeholder="Email"</label>
+            <label><button type="submit">Отправить</button></label>
+        </fieldset>
+    </form>
+    <script src="js/Java.js"></script>
+</body>
+</html>
---
+++ js/Java.js
@@ -0,0 +1,18 @@
+let number = [10, 167, 5, 68, 1, 28]
+number.sort(function (a,b) {
+    return a-b
+})
+console.log(number)//
+
+
+let num =[1, 5, 0, 2, 4 ,3]
+for (let i =0; i < 3 ; i++)
+   console.log(i)
+
+
+let users = [
+    { name: 'Коля', age: 18 }, { name: 'Вася', age: 12 }, { name: 'Степа', age: 20 }
+]
+users.forEach(user => {
+    if (user.age >= 18) console.log(user.name)
+})
